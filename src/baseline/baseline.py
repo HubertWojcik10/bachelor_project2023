aa0ab17c1261e434b7996bb2fdf4e409395df682
Hubert <hubert.wojcik002@gmail.com>
2024-02-03 15:43:55 +0100

    tokenize and create embeddings

--- src/baseline/baseline.py
+++ src/baseline/baseline.py
@@ -3,7 +3,7 @@ from data_preprocessing.train_data import RawTrainData
 import pandas as pd
 
 class Baseline:
-    def __init__(self):
+    def __init__(self) -> None:
         self.train_data = pd.read_csv("../data/train/train.csv")
         self.test_data = pd.read_csv("../data/test/final_test_pairs.csv")
 
