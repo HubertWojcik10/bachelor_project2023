ada8a94f456f0f1ece67a55dae1e91f293c642d3
Hubert <hubert.wojcik002@gmail.com>
2024-03-06 16:16:59 +0100

    chunk combinations without averaging

--- src/models/chunk_combinations/chunk_combinations.py
+++ src/models/chunk_combinations/chunk_combinations.py
@@ -9,8 +9,10 @@ import time
 from sklearn.model_selection import train_test_split
 import logging
 import os
+from utils.dev_utils import DevUtils
 from models.model import Model
-from utils.chunker import Chunker
+from utils.combinations_chunker import Chunker
+from collections import defaultdict
 
 
 class ChunkCombinationsModel(Model):
@@ -21,58 +23,76 @@ class ChunkCombinationsModel(Model):
         self.sep_token_id = self.tokenizer.sep_token_id
         self.params_dict = params_dict
         self.dev = dev
+        self.curr_time = curr_time
+        self.model_name = "chunk_combinations"
 
-    def split_data(self, df: pd.DataFrame):
+    def split_data(self, df: pd.DataFrame) -> Tuple[List[pd.DataFrame], List[pd.DataFrame]]:
         """
             Split the data into train and test
         """
+
+        # select the columns and split to train and validation
         df = df[["combinations", "overall"]]
-        train_df, val_df = train_test_split(df, test_size=(1- self.train_size), random_state=42)
+        train_df, val_df = train_test_split(df, test_size=(1- self.train_size), random_state=42, shuffle=False)
 
+        # split the train and validation data into batches
         batch_num = np.ceil(len(train_df) / self.batch_size)
-
         train_batched_data = np.array_split(train_df, batch_num)
         val_batched_data = np.array_split(val_df, batch_num)
 
         return train_batched_data, val_batched_data
     
-    def train(self, train_batched_data: List[pd.DataFrame], val_batched_data: List[pd.DataFrame], save_path: str):
+    def train(self, train_batched_data: List[pd.DataFrame], val_batched_data: List[pd.DataFrame], save_path: str) -> None:
         """
             Train the model
         """
         best_pearson = -1.0
+        losses = defaultdict(list)
         self.model.train()
 
         for epoch in range(self.epochs):
+            logging.info(f"{'-'*25} Epoch {epoch+1} of {self.epochs} {'-'*25}")
             start_time = time.time()
+
+            # iterate through the batches
             for _, batch in enumerate(train_batched_data):
                 combinations_list, labels_list = [], []
-                combinations, labels = batch["combinations"].values[1], batch["overall"].values
+                labels = batch["overall"].values
 
-                row, i = -1, -1
-                for key, combination in combinations.items():
-                    curr_row = key[0]
-                    if curr_row != row:
-                        row = curr_row
-                        i += 1
-                    label = labels[i]
-                    labels_list.append(float(label))
+                # iterate through the rows in the batch
+                for label, combinations in zip(labels, batch["combinations"]):
+                    # iterate through the combinations in the row
+                    for key, combination in combinations.items():
+                        labels_list.append(float(label))
 
-                    combinations_list.append(torch.tensor(combination, dtype=torch.float))
+                        combinations_list.append(torch.tensor(combination, dtype=torch.float))
 
+                # convert the lists to tensors and move them to the device
                 ids = torch.stack(combinations_list).long()
                 labels = torch.tensor(labels_list, dtype=torch.float).float()
-
                 ids, labels = ids.to(self.device), labels.to(self.device)
 
+                # forward pass
                 outputs = self.model(ids, labels=labels)
-                loss, _ = outputs[:2]
+                loss, logits = outputs[:2]
                 print(loss)
 
+                losses[epoch].append(loss.item())
+                self.optimizer.zero_grad()
                 loss.backward()
+                self.optimizer.step()
+
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+
             self.validate(save_path, start_time, val_batched_data, best_pearson)
 
-    def validate(self, save_path: str, start_time: float, val_data, best_pearson: float = -1.0):
+        # plot the loss
+        DevUtils.plot_loss(losses, self.model_name, self.curr_time)
+
+        # save the losses dictionary to a json file
+        DevUtils.save_losses_dict(losses, self.model_name, self.curr_time)
+
+    def validate(self, save_path: str, start_time: float, val_data, best_pearson: float = -1.0) -> None:
         """
             Validate the model
         """
@@ -81,7 +101,6 @@ class ChunkCombinationsModel(Model):
         dev_true, dev_pred, cur_pearson = self.predict(val_data, self.model)
 
         logging.info(f"Current dev pearson is {cur_pearson:.4f}, best pearson is {best_pearson:.4f}")
-        print(f"Current dev pearson is {cur_pearson:.4f}, best pearson is {best_pearson:.4f}")
 
         if cur_pearson > best_pearson:
             best_pearson = cur_pearson
@@ -89,9 +108,8 @@ class ChunkCombinationsModel(Model):
             torch.save(self.model.state_dict(), save_path)
 
         logging.info(f"Time costed : {round(time.time() - start_time, 3)}s")
-        print(f"Time costed : {round(time.time() - start_time, 3)}s \n")
 
-    def predict(self, data, model):
+    def predict(self, data, model) -> Tuple[List, List, float]:
         """
             Predict the scores
         """
@@ -99,30 +117,34 @@ class ChunkCombinationsModel(Model):
         model.eval()
         dev_true, dev_pred = [], []
 
+        # iterate through the batches
         for _, batch in enumerate(data):
             with torch.no_grad():
                 combinations_list, labels_list = [], []
-                combinations, labels = batch["combinations"].values[1], batch["overall"].values
+                labels = batch["overall"].values
 
-                row, i = -1, -1
-                for key, combination in combinations.items():
-                    curr_row = key[0]
-                    if curr_row != row:
-                        row = curr_row
-                        i += 1
-                    label = labels[i]
-                    labels_list.append(float(label))
+                # iterate through the rows in the batch
+                for label, combinations in zip(labels, batch["combinations"]):
 
-                    combinations_list.append(torch.tensor(combination, dtype=torch.float))
+                    # iterate through the combinations in the row
+                    for key, combination in combinations.items():
+                        labels_list.append(float(label))
 
+                        combinations_list.append(torch.tensor(combination, dtype=torch.float))
+
+                # convert the lists to tensors and move them to the device
                 ids, labels = torch.stack(combinations_list).long(), torch.tensor(labels_list, dtype=torch.float).float()
                 ids, labels = ids.to(self.device), labels.to(self.device)
 
+                # forward pass
                 outputs = model(ids, labels=labels)
-                loss, logits = outputs[:2]
+                _, logits = outputs[:2]
 
+                # append the true and predicted values to the lists
                 dev_true.extend(labels.cpu().detach().numpy())
                 dev_pred.extend(logits.cpu().detach().numpy().flatten())
+
+        # calculate the pearson correlation
         curr_pearson = np.corrcoef(dev_true, dev_pred)[0][1]
         logging.info(f"Finished prediction with pearson corr: {curr_pearson:.4f}")
         
