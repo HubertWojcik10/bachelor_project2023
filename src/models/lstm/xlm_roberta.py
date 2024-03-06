f60c4e1a43426b7698f32472023477c5f9e0aba4
German Buttiero <germa@MacBook-Air.local>
2024-03-06 15:22:19 +0100

    modified lstm

--- src/models/lstm/xlm_roberta.py
+++ src/models/lstm/xlm_roberta.py
@@ -13,7 +13,19 @@ class XLMRoberta(nn.Module):
     def __init__(self, model_name: str):
         super(XLMRoberta, self).__init__()
         self.model = XLMRobertaModel.from_pretrained(model_name)
-  
+    
+    def pad_to_same_size(self, tensors):
+        max_size = max(tensor.size(0) for tensor in tensors)
+        padded_tensors = []
+        for tensor in tensors:
+            if tensor.size(0) < max_size:
+                padding = torch.zeros(max_size - tensor.size(0), tensor.size(1))
+                padded_tensor = torch.cat((tensor, padding), dim=0)
+                padded_tensors.append(padded_tensor)
+            else:
+                padded_tensors.append(tensor)
+        return torch.stack(padded_tensors)
+    
     def run(self, input_ids: List[List[List[Tensor]]]):
         output = []
         for i in range(len(input_ids)):
@@ -21,15 +33,16 @@ class XLMRoberta(nn.Module):
             row_emb = []
             for text in input_ids[i]:
                 #iterates through each article per row
-                chunks_emb = []
+                text_emb = []
                 for chunk in text:
                     chunk_ids = torch.tensor(chunk).unsqueeze(0)
                     attention_mask = [1 if i != 0 else 0 for i in chunk_ids.tolist()]
                     attention_mask = torch.tensor(attention_mask).unsqueeze(0)
                     outputs= self.model(chunk_ids, attention_mask)
                     last_hidden_state = outputs.last_hidden_state.mean(dim=1).squeeze(0)
-                    chunks_emb.append(last_hidden_state)
-                chunks_emb = torch.stack(chunks_emb).unsqueeze(0)                
-                row_emb.append(chunks_emb)
+                    text_emb.append(last_hidden_state)
+                text_emb = torch.stack(text_emb)             
+                row_emb.append(text_emb)
+            row_emb = self.pad_to_same_size(row_emb)
             output.append(row_emb)
         return output
